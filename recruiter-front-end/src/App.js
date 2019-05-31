66c877a7eb21c47c2a56c9e1eeaac97a7a94ca84
Duc Vu <de.yd315@gmail.com>
2019-05-31 13:55:41 -0500

    added links to specific user profile

--- recruiter-front-end/src/App.js
+++ recruiter-front-end/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./App.css";
 import Users from "./components/Users";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import User from "./components/User";
+
 
 class App extends React.Component {
   render() {
+    // console.log(this.props)
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Recruiter Rule</h1>
-        </header>
-        <Users />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Recruiter Rule</h1>
+          </header>
+          <Route exact path='/' component={Users} />
+          <Route exact path='/:id' component={User} />
+          {/* <Users /> */}
+        </div>
+      </Router>
     );
   }
 }
--- recruiter-front-end/src/components/User/index.js
+++ recruiter-front-end/src/components/User/index.js
@@ -7,8 +7,8 @@ class User extends React.Component {
         <h1>
           {this.props.user.first_name} {this.props.user.last_name}
         </h1>
-        <h2>{this.props.user.email}</h2>
-        <h2>{this.props.user.phone_number}</h2>
+        {/* <h2>{this.props.user.email}</h2>
+        <h2>{this.props.user.phone_number}</h2> */}
       </div>
     );
   }
