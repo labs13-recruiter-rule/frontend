cff99371118d84692377cbf2aa3c65c09ac9e766
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-07 15:20:13 -0500

    Update NewRule.js

--- recruiter-front-end/src/components/NewRule/NewRule.js
+++ recruiter-front-end/src/components/NewRule/NewRule.js
@@ -1,69 +1,19 @@
 /* IMPORTS */
 import React from 'react'
-import { render } from 'react-dom'
-import {
-  Button,
-  Container,
-  Divider,
-  Header
-} from 'semantic-ui-react'
+import { Container, Divider, Header, Button } from 'semantic-ui-react';
 /* IMPORTS */
 
 class NewRule extends React.Component {
-  componentDidMount() {
-    const selection = document.createElement('div')
-    selection.classList.add('ui-selection')
-    document.body.appendChild(selection)
-
-    let currentElement
-
-    const handleIndicatorMouse = function(e) {
-      if (e.target.tagName === 'I' || e.target.classList.contains('ui')) {
-        currentElement = e.target
-      }
-
-      const {
-        top,
-        left,
-        width,
-        height,
-      } = currentElement.getBoundingClientRect()
-
-      if (e.type === 'mouseover') {
-        selection.setAttribute(
-          'data-component',
-          currentElement.className.split(' ').join('.'),
-        )
-        selection.classList.add('visible')
-        selection.style.top = top + 'px'
-        selection.style.left = left + 'px'
-        selection.style.width = width + 'px'
-        selection.style.height = height + 'px'
-      } else {
-        selection.classList.remove('visible')
-      }
-
-      e.stopPropagation()
-    }
-  }
-
   render() {
     return (
       <Container>
-        <Header size='huge'>Recruiter Rule</Header>
-        <Divider hidden />
-        <div className="tipText">
-          <p>No rules setup. Create your first rule.</p>
-        </div>
-        <Divider hidden />
-        <Button
-          content="Create Rule"
-          href="#"
-          target="_blank"
-        />
-        <Divider hidden clearing />
+        <Header as='h1'>Recruiter Rule</Header>
+        <Divider hidden/>
+        <p>No rules setup. Create your first rule.</p>
+        <Divider hidden/>
+        <Button class="ui button">Create Rule</Button>
       </Container>
-    )
+    );
   }
 }
 
