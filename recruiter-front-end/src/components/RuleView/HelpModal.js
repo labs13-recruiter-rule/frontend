193727f25248855029e88a961c07dd89fb623beb
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-10 18:24:44 -0500

    Create HelpModal.js

---
+++ recruiter-front-end/src/components/RuleView/HelpModal.js
@@ -0,0 +1,42 @@
+/* IMPORTS */
+import React from 'react';
+import { Header, Modal, Divider, Button, Grid } from "semantic-ui-react";
+/* IMPORTS */
+
+export default class HelpModal extends React.Component {
+  render() {
+    return (
+      <Grid>
+        <Grid.Column textAlign="center">
+          <Modal trigger={<Button basic color='blue'>Show Modal</Button>}>
+            <Modal.Header>Help Modal</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                <Header>
+                  Help Topic 1
+                </Header>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius duis at consectetur lorem donec massa sapien faucibus. Eros in cursus turpis massa tincidunt dui ut ornare. Id interdum velit laoreet id donec ultrices tincidunt arcu non. Sagittis purus sit amet volutpat consequat.
+                </p>
+                <Divider hidden/>
+                <Header>
+                  Help Topic 2
+                </Header>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius duis at consectetur lorem donec massa sapien faucibus. Eros in cursus turpis massa tincidunt dui ut ornare. Id interdum velit laoreet id donec ultrices tincidunt arcu non. Sagittis purus sit amet volutpat consequat.
+                </p>
+                <Divider hidden/>
+                <Header>
+                  Help Topic 3
+                </Header>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius duis at consectetur lorem donec massa sapien faucibus. Eros in cursus turpis massa tincidunt dui ut ornare. Id interdum velit laoreet id donec ultrices tincidunt arcu non. Sagittis purus sit amet volutpat consequat.
+                </p>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
