5444f0648eeb872f4df90028d1f2d5cd53e3242b
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-11 12:44:56 -0500

    Update RuleMain.js

--- recruiter-front-end/src/components/RuleView/RuleMain.js
+++ recruiter-front-end/src/components/RuleView/RuleMain.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import MainHeader from './MainHeader';
 import HelpModal from './HelpModal';
 import CreateCondition from './CreateCondition';
-import { Container, Divider, Form } from 'semantic-ui-react';
+import { Container, Divider } from 'semantic-ui-react';
 /* IMPORTS */
 
 export class RuleMain extends Component {
@@ -11,26 +11,15 @@ export class RuleMain extends Component {
   render() {
     return (
       <Container>
-        <Form>
-
-        </Form>
+        <Divider hidden />
+        <MainHeader />
+        <Divider hidden/>
+        <CreateCondition />
+        <Divider hidden />
+        <HelpModal />
       </Container>
     )
   }
 }
 
 export default RuleMain
-
-/*
-https://balsamiq.cloud/s5supw2/ppt820q/rAEEC
-https://react.semantic-ui.com/modules/tab/#types-basic-all
-
-
-FIRST VIEW
-<Divider hidden />
-<MainHeader />
-<Divider hidden/>
-<CreateCondition />
-<Divider hidden />
-<HelpModal />
-*/
