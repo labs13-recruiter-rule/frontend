f6a2b84c2f0b9910bbf39ef24ab9da5cb9cbe805
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-11 12:44:49 -0500

    Update CreateCondition.js

--- recruiter-front-end/src/components/RuleView/CreateCondition.js
+++ recruiter-front-end/src/components/RuleView/CreateCondition.js
@@ -1,18 +1,16 @@
 /* IMPORTS */
-import React from 'react';
-import { Grid, Button } from "semantic-ui-react";
+import React from 'react'
+import { Button, Grid } from 'semantic-ui-react'
 /* IMPORTS */
 
-export default class CreateCondition extends React.Component {
-  render() {
-    return (
-      <Grid>
-          <Grid.Column textAlign="center">
-              <Button size='massive'>
-                Create a Condition
-              </Button>
-          </Grid.Column>
-        </Grid>
-    )
-  }
-}
+const CreateCondition = () => (
+<Grid>
+  <Grid.Column textAlign="center">
+    <Button size='massive'>
+      Create a Condition
+    </Button>
+  </Grid.Column>
+</Grid>
+)
+
+export default CreateCondition
