1d896d74148e3febec5b610687a9b24c7d600ef1
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-11 12:44:54 -0500

    Update MainHeader.js

--- recruiter-front-end/src/components/RuleView/MainHeader.js
+++ recruiter-front-end/src/components/RuleView/MainHeader.js
@@ -1,14 +1,12 @@
 /* IMPORTS */
-import React from 'react';
-import { Header } from "semantic-ui-react";
+import React from 'react'
+import { Header } from 'semantic-ui-react'
 /* IMPORTS */
 
-export default class MainHeader extends React.Component {
-  render() {
-    return (
-      <Header as='h2' disabled textAlign='center'>
-        No conditions created. Click to create conditions.
-      </Header>
-    )
-  }
-}
+const MainHeader = () => (
+  <Header as='h2' disabled textAlign='center'>
+    No conditions created. Click to create conditions.
+  </Header>
+)
+
+export default MainHeader
