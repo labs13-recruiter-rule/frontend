01f917b48c3fe0d41f2976dbcd334d73451b4c92
Timothy Shores <timothymshores@gmail.com>
2019-06-13 12:59:14 -0500

    Resolved merge conflicts

--- recruiter-front-end/src/App.js
+++ recruiter-front-end/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Users from './components/Users';
 import Dashboard from './components/Dashboard';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Login from './components/Login';
 //! import User from "./components/User"; DEFINED BUT NEVER USED
 import { connect } from 'react-redux';
+import NewUserLandingPage from './views/NewUserLandingPage/NewUserLandingPage';
+import NewContactGroup from './views/NewContactGroup/NewContactGroup';
 import NewCandidate from './components/NewCandidate/NewCandidate';
 import RuleMain from './components/RuleView/RuleMain';
 import Contacts from './components/Contacts/Contacts';
@@ -35,73 +37,73 @@ class App extends React.Component {
     });
   }
 
-	logout() {
-		fire.auth().signOut();
-	}
+  logout() {
+    fire.auth().signOut();
+  }
 
-	render() {
-		return (
-			<Container>
-				<Router>
-					{this.state.user ? (
-						[
-							<>
-								<Menu>
-									<Menu.Item>
-										<Button>
-											<NavLink style={{ color: "rgba(0,0,0,.6)" }} to="/">
-												Home
-											</NavLink>
-										</Button>
-									</Menu.Item>
-									<Menu.Item>
-										<Button>
-											<NavLink
-												style={{ color: "rgba(0,0,0,.6)" }}
-												to="/new-candidate"
-											>
-												New Candidate
-											</NavLink>
-										</Button>
-									</Menu.Item>
-									<Menu.Item>
-										<Button>
-											<NavLink style={{ color: "rgba(0,0,0,.6)" }} to="/mailer">
-												Mailer
-											</NavLink>
-										</Button>
-									</Menu.Item>
-									<Menu.Item position="right">
-										<Button onClick={this.logout}>logout</Button>
-									</Menu.Item>
-								</Menu>
-								<Route exact path="/" component={NewUserLandingPage} />
-								<Route exact path="/db" component={Dashboard} />
-								<Route
-									exact
-									path="/id"
-									render={props => {
-										console.log(props);
-										return <div>UserId: {props.match.params.id}</div>;
-									}}
-								/>
-								<Route exact path="/new-candidate" component={NewCandidate} />
-								<Route exact path="/contacts" component={Contacts} />
-								<Route exact path="/mailer" component={Mailer} />
-								<Route
-									exact
-									path="/new-contact-group"
-									component={NewContactGroup}
-								/>
-							</>
-						]
-					) : (
-						<Login />
-					)}
-				</Router>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container>
+        <Router>
+          {this.state.user ? (
+            [
+              <>
+                <Menu>
+                  <Menu.Item>
+                    <Button>
+                      <NavLink style={{ color: 'rgba(0,0,0,.6)' }} to="/">
+                        Home
+                      </NavLink>
+                    </Button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Button>
+                      <NavLink
+                        style={{ color: 'rgba(0,0,0,.6)' }}
+                        to="/new-candidate"
+                      >
+                        New Candidate
+                      </NavLink>
+                    </Button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Button>
+                      <NavLink style={{ color: 'rgba(0,0,0,.6)' }} to="/mailer">
+                        Mailer
+                      </NavLink>
+                    </Button>
+                  </Menu.Item>
+                  <Menu.Item position="right">
+                    <Button onClick={this.logout}>logout</Button>
+                  </Menu.Item>
+                </Menu>
+                <Route exact path="/" component={NewUserLandingPage} />
+                <Route exact path="/db" component={Dashboard} />
+                <Route
+                  exact
+                  path="/id"
+                  render={props => {
+                    console.log(props);
+                    return <div>UserId: {props.match.params.id}</div>;
+                  }}
+                />
+                <Route exact path="/new-candidate" component={NewCandidate} />
+                <Route exact path="/contacts" component={Contacts} />
+                <Route exact path="/mailer" component={Mailer} />
+                <Route
+                  exact
+                  path="/new-contact-group"
+                  component={NewContactGroup}
+                />
+              </>,
+            ]
+          ) : (
+            <Login />
+          )}
+        </Router>
+      </Container>
+    );
+  }
 }
 
 const mapStateToProps = state => {
