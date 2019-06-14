e662f29a999ed8932d85538043ddc6566379ac42
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-14 14:08:14 -0500

    Create WhatModal.js

---
+++ recruiter-front-end/src/views/CreateDefaultContact/WhatModal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, Modal } from 'semantic-ui-react';
+
+const WhatModal = () => (
+  <Modal trigger={<Button>What is a Default Contact?</Button>}>
+    <Modal.Header>What is a Default Contact</Modal.Header>
+    <Modal.Content>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Pellentesque
+        pulvinar pellentesque habitant morbi tristique senectus et netus et. Eu
+        ultrices vitae auctor eu augue ut lectus arcu.
+      </p>
+    </Modal.Content>
+  </Modal>
+)
+
+export default WhatModal
