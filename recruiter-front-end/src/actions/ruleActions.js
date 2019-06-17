605b62931ae6876007d4070ddb6f8a5600cc4577
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-06-17 17:55:19 -0500

    updated reducers

--- recruiter-front-end/src/actions/ruleActions.js
+++ recruiter-front-end/src/actions/ruleActions.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
-
+import {
+  GET_RULES_FAIL,
+  GET_RULES_SUCCESS,
+  GET_RULE_SUCCESS,
+  GET_RULE_FAIL,
+  ADD_RULE_SUCCESS,
+  ADD_RULE_FAIL,
+  UPDATE_RULE_SUCCESS,
+  UPDATE_RULE_FAIL,
+  DELETE_RULE_SUCCESS,
+  DELETE_RULE_FAIL,
+} from './types';
 // TOKEN
 
 const token = sessionStorage.getItem('token');
@@ -10,9 +21,6 @@ const tokenHeader = {
 };
 
 // GET RULES
-
-export const GET_RULES_SUCCESS = 'GET_RULES_SUCCESS';
-export const GET_RULES_FAIL = 'GET_RULES_FAIL';
 
 export const getRules = url => dispatch => {
   return axios
@@ -31,8 +39,75 @@ export const getRules = url => dispatch => {
     });
 };
 
+// GET RULE BY ID
+export const getRule = url => dispatch => {
+  return axios
+    .get(`${url}`, tokenHeader)
+    .then(res => {
+      dispatch({
+        type: GET_RULE_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_RULE_FAIL,
+        payload: error,
+      });
+    });
+};
+
 // ADD RULE
+export const addRule = (url, newRule) => dispatch => {
+  return axios
+    .post(`${url}`, newRule, tokenHeader)
+    .then(res => {
+      dispatch({
+        type: ADD_RULE_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: ADD_RULE_FAIL,
+        payload: error,
+      });
+    });
+};
 
 // UPDATE RULE
 
+export const updateRule = (url, updatedRule) => dispatch => {
+  return axios
+    .put(`${url}`, updatedRule, tokenHeader)
+    .then(res => {
+      dispatch({
+        type: UPDATE_RULE_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: UPDATE_RULE_FAIL,
+        payload: error,
+      });
+    });
+};
+
 // DELETE RULE
+export const deleteRule = url => dispatch => {
+  return axios
+    .delete(`${url}`, tokenHeader)
+    .then(res => {
+      dispatch({
+        type: DELETE_RULE_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: DELETE_RULE_FAIL,
+        payload: error,
+      });
+    });
+};
--- recruiter-front-end/src/reducers/index.js
+++ recruiter-front-end/src/reducers/index.js
@@ -1,18 +1,89 @@
 import {
-  GET_USERS_SUCCESS,
   GET_USERS_FAIL,
-  GET_USER_SUCCESS,
+  GET_USERS_SUCCESS,
   GET_USER_FAIL,
-  GET_USER_ID_SUCCESS,
+  GET_USER_SUCCESS,
   GET_USER_ID_FAIL,
-  ADD_USER_SUCCESS,
+  GET_USER_ID_SUCCESS,
   ADD_USER_FAIL,
+  ADD_USER_SUCCESS,
+  GET_CONTACTS_SUCCESS,
+  GET_CONTACTS_FAIL,
+  GET_CONTACT_SUCCESS,
+  GET_CONTACT_FAIL,
+  ADD_CONTACT_SUCCESS,
+  ADD_CONTACT_FAIL,
+  UPDATE_CONTACT_FAIL,
+  UPDATE_CONTACT_SUCCESS,
+  DELETE_CONTACT_FAIL,
+  DELETE_CONTACT_SUCCESS,
+  CREATE_GROUP_FAIL,
+  CREATE_GROUP_SUCCESS,
+  EDIT_GROUP_FAIL,
+  EDIT_GROUP_SUCCESS,
+  DELETE_GROUP_FAIL,
+  DELETE_GROUP_SUCCESS,
+  GET_GROUP_FAIL,
+  GET_GROUP_SUCCESS,
+  ADD_CONTACT_TO_GROUP_FAIL,
+  ADD_CONTACT_TO_GROUP_SUCCESS,
+  REMOVE_CONTACT_FROM_GROUP_FAIL,
+  REMOVE_CONTACT_FROM_GROUP_SUCCESS,
+  EDIT_CONTACT_IN_GROUP_FAIL,
+  EDIT_CONTACT_IN_GROUP_SUCCESS,
+  GET_RULES_FAIL,
+  GET_RULES_SUCCESS,
+  GET_RULE_SUCCESS,
+  GET_RULE_FAIL,
+  ADD_RULE_SUCCESS,
+  ADD_RULE_FAIL,
+  UPDATE_RULE_SUCCESS,
+  UPDATE_RULE_FAIL,
+  DELETE_RULE_SUCCESS,
+  DELETE_RULE_FAIL,
+  GET_ENGINES_FAIL,
+  GET_ENGINES_SUCCESS,
+  GET_ENGINE_FAIL,
+  GET_ENGINE_SUCCESS,
+  ADD_ENGINE_FAIL,
+  ADD_ENGINE_SUCCESS,
+  UPDATE_ENGINE_FAIL,
+  UPDATE_ENGINE_SUCCESS,
+  DELETE_ENGINE_FAIL,
+  DELETE_ENGINE_SUCCESS,
+  ADD_EMAIL_HISTORY_SUCCESS,
+  ADD_EMAIL_HISTORY_FAIL,
+  GET_EMAIL_HISTORY_SUCCESS,
+  GET_EMAIL_HISTORY_FAIL,
+  GET_EMAIL_TOTAL_SUCCESS,
+  GET_EMAIL_TOTAL_FAIL,
+  GET_EMAILS_BY_CANDIDATE_FAIL,
+  GET_EMAILS_BY_CANDIDATE_SUCCESS,
+  GET_EMAILS_BY_CONTACT_FAIL,
+  GET_EMAILS_BY_CONTACT_SUCCESS,
+  SEND_EMAIL_SUCCESS,
+  SEND_EMAIL_FAIL,
 } from '../actions';
 
 const initialState = {
   users: [],
   user: {},
   error: null,
+  contact: {},
+  contacts: [],
+  groupContacts: [],
+  groupContact: {},
+  engine: {},
+  engines: [],
+  rule: {},
+  rules: [],
+  email_history: [],
+  email_total: null,
+  email_total_candidate: null,
+  email_total_contact: null,
+  group: {},
+  groups: [],
+  message: '',
 };
 
 const reducer = (state = initialState, action) => {
@@ -55,6 +126,276 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         user_id: action.payload,
+      };
+    case GET_CONTACTS_SUCCESS:
+      return {
+        ...state,
+        contacts: action.payload,
+      };
+    case GET_CONTACTS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_CONTACT_SUCCESS:
+      return {
+        ...state,
+        contact: action.payload,
+      };
+    case GET_CONTACT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_CONTACT_SUCCESS:
+      return {
+        ...state,
+        contact: action.payload,
+      };
+    case ADD_CONTACT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case UPDATE_CONTACT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case UPDATE_CONTACT_SUCCESS:
+      return {
+        ...state,
+        contact: action.payload,
+      };
+    case DELETE_CONTACT_SUCCESS:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case DELETE_CONTACT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case CREATE_GROUP_SUCCESS:
+      return {
+        ...state,
+        group: action.payload,
+      };
+    case CREATE_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case EDIT_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case EDIT_GROUP_SUCCESS:
+      return {
+        ...state,
+        group: action.payload,
+      };
+    case DELETE_GROUP_SUCCESS:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case DELETE_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_GROUP_SUCCESS:
+      return {
+        ...state,
+        group: action.payload,
+      };
+    case ADD_CONTACT_TO_GROUP_SUCCESS:
+      return {
+        ...state,
+        groupContact: action.payload,
+      };
+    case ADD_CONTACT_TO_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case REMOVE_CONTACT_FROM_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case REMOVE_CONTACT_FROM_GROUP_SUCCESS:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case EDIT_CONTACT_IN_GROUP_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case EDIT_CONTACT_IN_GROUP_SUCCESS:
+      return {
+        ...state,
+        groupContact: action.payload,
+      };
+    case GET_RULES_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_RULES_SUCCESS:
+      return {
+        ...state,
+        rules: action.payload,
+      };
+    case GET_RULE_SUCCESS:
+      return {
+        ...state,
+        rule: action.payload,
+      };
+    case GET_RULE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_RULE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_RULE_SUCCESS:
+      return {
+        ...state,
+        rule: action.payload,
+      };
+    case UPDATE_RULE_SUCCESS:
+      return {
+        ...state,
+        rule: action.payload,
+      };
+    case UPDATE_RULE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case DELETE_RULE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case DELETE_RULE_SUCCESS:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    case GET_ENGINES_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_ENGINES_SUCCESS:
+      return {
+        ...state,
+        engines: action.payload,
+      };
+    case GET_ENGINE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_ENGINE_SUCCESS:
+      return {
+        ...state,
+        engine: action.payload,
+      };
+    case ADD_ENGINE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_ENGINE_SUCCESS:
+      return {
+        ...state,
+        engine: action.payload,
+      };
+    case UPDATE_ENGINE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case UPDATE_ENGINE_SUCCESS:
+      return {
+        ...state,
+        engine: action.payload,
+      };
+    case DELETE_ENGINE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case DELETE_ENGINE_SUCCESS:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_EMAIL_HISTORY_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_EMAIL_HISTORY_SUCCESS:
+      return {
+        ...state,
+        email_history: action.payload,
+      };
+    case GET_EMAIL_HISTORY_SUCCESS:
+      return {
+        ...state,
+        email_history: action.payload,
+      };
+    case GET_EMAIL_HISTORY_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_EMAIL_TOTAL_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_EMAIL_TOTAL_SUCCESS:
+      return {
+        ...state,
+        email_total: action.payload,
+      };
+    case GET_EMAILS_BY_CANDIDATE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_EMAILS_BY_CANDIDATE_SUCCESS:
+      return {
+        ...state,
+        email_total_candidate: action.payload,
+      };
+    case GET_EMAILS_BY_CONTACT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_EMAILS_BY_CONTACT_SUCCESS:
+      return {
+        ...state,
+        email_total_contact: action.payload,
       };
     default:
       return state;
