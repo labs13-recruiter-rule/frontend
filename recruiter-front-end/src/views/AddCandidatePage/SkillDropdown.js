6227869e5be4af5c262cccf64c75f1e53b7a7c3e
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-18 17:32:52 -0500

    Update SkillDropdown.js

--- recruiter-front-end/src/views/AddCandidatePage/SkillDropdown.js
+++ recruiter-front-end/src/views/AddCandidatePage/SkillDropdown.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Dropdown } from 'semantic-ui-react';
+import React from './node_modules/react';
+import { Dropdown } from './node_modules/semantic-ui-react';
 
 const skills = [
   { key: 'Adaptability', text: 'Adaptability', value: 'Adaptability' },
