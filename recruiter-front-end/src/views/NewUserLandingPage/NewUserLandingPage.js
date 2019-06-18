48f216f4b922959d89c3c8501aa1e578331c567b
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-18 17:33:18 -0500

    Update NewUserLandingPage.js

--- recruiter-front-end/src/views/NewUserLandingPage/NewUserLandingPage.js
+++ recruiter-front-end/src/views/NewUserLandingPage/NewUserLandingPage.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Grid, Button, Modal, Header } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import React from "./node_modules/react";
+import { Grid, Button, Modal, Header } from "./node_modules/semantic-ui-react";
+import { Link } from "./node_modules/react-router-dom";
 
 const flexContainer = {
 	display: "flex",
