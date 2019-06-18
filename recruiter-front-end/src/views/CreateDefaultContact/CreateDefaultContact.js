b899f7f319f251ca6c9715c441a559439717eb46
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-18 10:18:55 -0500

    Update CreateDefaultContact.js

--- recruiter-front-end/src/views/CreateDefaultContact/CreateDefaultContact.js
+++ recruiter-front-end/src/views/CreateDefaultContact/CreateDefaultContact.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Grid, Divider, Form, Input, Container } from 'semantic-ui-react';
+import { Grid, Divider } from 'semantic-ui-react';
+import DefaultContactMessage from './DefaultContactMessage';
+import DefaultContactFields from './DefaultContactFields';
+import WhatModal from './WhatModal';
+import CreateRuleButton from './CreateRuleButton';
 
 const flexContainer = {
   display: 'flex',
@@ -12,12 +16,24 @@ const center = {
 
 function CreateDefaultContact() {
   return (
-    <Container>
-      <h2 className="ui header" style={center}>
-        Default Contact
-      </h2>
-      <Form></Form>
-    </Container>
+    <Grid columns={12}>
+      <Grid.Row centered>
+        <Grid.Column width={1} />
+        <Grid.Column width={10} centered style={flexContainer}>
+          <h1 className="ui header" style={center}>
+            Default Contact
+          </h1>
+          <Divider />
+          <DefaultContactMessage style={center} />
+          <Divider hidden />
+          <DefaultContactFields />
+          <Divider hidden />
+          <WhatModal centered />
+          <Divider hidden />
+          <CreateRuleButton />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
