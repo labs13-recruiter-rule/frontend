44090d0c26f6e5e6b071d2d8e13dc9b14bd3f30d
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-19 15:56:26 -0500

    Update App.js

--- recruiter-front-end/src/App.js
+++ recruiter-front-end/src/App.js
@@ -16,7 +16,7 @@ import { Menu, Button, Container } from 'semantic-ui-react';
 import history from './history';
 import CreateDefaultContact from './views/CreateDefaultContact/CreateDefaultContact';
 import AddCandidatePage from './views/AddCandidatePage/AddCandidatePage'
-import ConfirmationView from './views/ConfirmationPage/ConfirmationView';
+import Confirmation from './views/NewRulesPage/Confirmation';
 
 class App extends React.Component {
   state = {
@@ -98,7 +98,7 @@ class App extends React.Component {
                 <Route
                   exact
                   path="/testing"
-                  component={ConfirmationView}
+                  component={Confirmation}
                 />
               </>,
             ]
