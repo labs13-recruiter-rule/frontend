780943d6ee37b9bf01831b72462bc4c99e037540
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-19 12:27:13 -0500

    Update ConfirmationView.js

--- recruiter-front-end/src/views/ConfirmationPage/ConfirmationView.js
+++ recruiter-front-end/src/views/ConfirmationPage/ConfirmationView.js
@@ -1,24 +1,18 @@
 import React from './node_modules/react';
-import {
-  Grid,
-  Button,
-  Modal,
-  Header,
-  Icon,
-  Progress,
-  Step,
-  Dropdown,
-  Divider,
-} from './node_modules/semantic-ui-react';
-import { Link } from './node_modules/react-router-dom';
-import Axios from './node_modules/axios';
-
-const token = sessionStorage.getItem('token');
+import { Container } from './node_modules/semantic-ui-react';
+import Message from './Message';
 
 class ConfirmationView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   render() {
     return (
+      <Container>
+        <Message />
+      </Container>
     );
   }
 }
