9ed909883420c12a9b41a542c68953c91db90f4c
Brandon Desselle <desselle.brandon2@gmail.com>
2019-06-19 11:58:53 -0500

    Create ConfirmationView.js

---
+++ recruiter-front-end/src/views/ConfirmationPage/ConfirmationView.js
@@ -0,0 +1,26 @@
+import React from './node_modules/react';
+import {
+  Grid,
+  Button,
+  Modal,
+  Header,
+  Icon,
+  Progress,
+  Step,
+  Dropdown,
+  Divider,
+} from './node_modules/semantic-ui-react';
+import { Link } from './node_modules/react-router-dom';
+import Axios from './node_modules/axios';
+
+const token = sessionStorage.getItem('token');
+
+class ConfirmationView extends React.Component {
+
+  render() {
+    return (
+    );
+  }
+}
+
+export default ConfirmationView;
